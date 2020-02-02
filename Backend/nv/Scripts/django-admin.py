79a7f9149b82096456edb530c3b547da3e73a089
Venk677 <venkatesh.vk677@gmail.com>
2020-02-03 00:15:59 +0530

    Have to install docker for channels

---
+++ Backend/nv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\venkatesh\desktop\projects\chat-application\backend\nv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
